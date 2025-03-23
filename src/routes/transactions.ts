e55fe5bd6eba69c0c49129e486744a8fa6e0bcb9
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-03-23 13:57:55 -0300

    feat: add new route to get transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,8 +1,22 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
-import { z } from 'zod'
+import { string, z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParmsSchema = z.object({
+      id: string().uuid(),
+    })
+    const { id } = getTransactionParmsSchema.parse(request.params)
+    const transaction = await knex('transactions').where('id', id).first()
+    return { transaction }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionSchema = z.object({
       title: z.string(),
